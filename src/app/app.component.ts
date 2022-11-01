669ac35638b4cc21ae305fe61cfc961f5e700e1c
luongtx <luong.tranxuan@emea.nttdata.com>
2022-11-01 15:01:56 +0700

    update model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { CfgColumn } from './cfg-column.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
- itemStringsLeft: any[] = [
-    'Windstorm',
-    'Bombasto',
-    'Magneta',
-    'Tornado'
-  ];
+export class AppComponent {
+
+  columnsConfig: CfgColumn[] = [
+    { title: 'Kundennetzbezeichnung', property: 'designation', markAsLink: true },
+    { title: 'Technische Teilnetze', property: 'technicalSubnets' },
+    { title: 'Produkte', property: 'products' },
+    { title: 'TDN-Vertragsnr.', property: 'tdnContractNo' },
+    { title: 'ZGSL-Nr.', property: 'zgslNo' },
+    { title: 'Gesamtpreis monatlich', property: 'monthlyTotalPrice' },
+    { title: 'Vertragsbeginn', property: 'contractStartDate' },
+    { title: 'Vertragslaufzeit', property: 'contractDuration' },
+  ]
+
 }
 
 
---
+++ src/app/cfg-column.model.ts
@@ -0,0 +1,8 @@
+export interface CfgColumn {
+	title: string,
+	property: string,
+	searchable?: boolean,
+	sortable?: boolean,
+	width?: number, 
+	markAsLink?: boolean
+}
